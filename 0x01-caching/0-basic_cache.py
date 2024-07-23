0c38cd401e93678767ff22127bbb75e6f4f2a86b
O-G-W-A-L <ogwalja04@gmail.com>
2024-07-23 06:40:45 +0000

    style fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,6 +3,7 @@
 """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """Represents an object that allows storing and
     retrieving items from a dictionary without any limit.
@@ -10,11 +11,9 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Adds an item in the cache.
-        
         Args:
             key (str): The key under which the item will be stored.
             item (any): The item to be stored in the cache.
-        
         Returns:
             None: If either key or item is None, the method does nothing.
         """
@@ -24,10 +23,8 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Retrieves an item by key from the cache.
-        
         Args:
             key (str): The key corresponding to the item to be retrieved.
-        
         Returns:
             any: The item stored in the cache with the specified key.
             None: If the key is None or the key does not exist in the cache.
